d54bfc26f767709adca0c9568663ec906aa748a0
Elizabeth Lu <ezlofficial2019@gmail.com>
2024-01-07 16:52:30 -0800

    start again

--- Develop/script.js
+++ Develop/script.js
@@ -18,15 +18,15 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-    //make arrays
-    //present a series of prompy
-    //choose length 8-128
-    //ask whether or not to include at least one lowercase
-    //ask whether or not to include at least one uppercase
-    //ask whether or not to include at least one numeric
-    //ask whether or not to include at least one special characters
-    //generate password characters "length" times from random categories
-    //check if all criterias are met
-    //insert new random characters and delete old characters until all criterias are met
-    //run functions and display the password
+  //make arrays
+  //present a series of prompy
+  //choose length 8-128
+  //ask whether or not to include at least one lowercase
+  //ask whether or not to include at least one uppercase
+  //ask whether or not to include at least one numeric
+  //ask whether or not to include at least one special characters
+  //generate password characters "length" times from random categories
+  //check if all criterias are met
+  //insert new random characters and delete old characters until the criteria is met
+  //run functions and display the password
 }
